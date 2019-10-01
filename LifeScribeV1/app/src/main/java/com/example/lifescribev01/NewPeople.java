53ac120c648f545f8f33128d33589f4dcded54ab
amcneil <amcneil@mymail.mines.edu>
2019-10-01 10:51:43 -0600

    Added a submit button to the people creation page that stores the data in variables

--- LifeScribeV1/app/src/main/java/com/example/lifescribev01/NewPeople.java
+++ LifeScribeV1/app/src/main/java/com/example/lifescribev01/NewPeople.java
@@ -1,5 +1,6 @@
 package com.example.lifescribev01;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -9,10 +10,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 public class NewPeople extends AppCompatActivity {
-
+    String name, DOB, DOD, bio;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,8 +22,22 @@ public class NewPeople extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-    }
 
-    EditText DOB = findViewById(R.id.DOB);
-    
+        Button fab = findViewById(R.id.submit);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final EditText nameField = (EditText) findViewById(R.id.name);
+                name = nameField.getText().toString();
+                final EditText dobField = (EditText) findViewById(R.id.DOB);
+                DOB = dobField.getText().toString();
+                final EditText dodField = (EditText) findViewById(R.id.DOD);
+                DOD = dobField.getText().toString();
+                final EditText bioField = (EditText) findViewById(R.id.bio);
+                bio = dobField.getText().toString();
+                //startActivity(new Intent(NewPeople.this, PeopleActivity.class));
+                finish();
+            }
+        });
+    }
 }
