60bd5078cd2244898d16ea0410c367eb174d339e
Daniel Thorne <thorne@mymail.mines.edu>
2019-10-26 16:51:46 -0600

    added 2 more tables :)

---
+++ LifeScribeV1/app/src/main/java/com/example/lifescribev01/database/SiblingXRefDao.java
@@ -0,0 +1,16 @@
+package com.example.lifescribev01.database;
+
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.Query;
+
+import java.util.List;
+
+@Dao
+public interface SiblingXRefDao {
+    @Insert
+    void insert(SiblingXRef pair);
+
+    @Query("SELECT * FROM person INNER JOIN SIBLING_XREF ON person.person_id = sibling_xref.sibling_2_id WHERE sibling_xref.sibling_1_id=:siblingID")
+    List<Person> getParentsOfPerson(int siblingID);
+}
---
+++ LifeScribeV1/app/src/main/java/com/example/lifescribev01/database/Story.java
@@ -0,0 +1,29 @@
+package com.example.lifescribev01.database;
+
+import androidx.annotation.NonNull;
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity
+public class Story {
+    @NonNull
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "story_id")
+    public int storyID;
+
+    @ColumnInfo (name = "title")
+    public String title;
+
+    @ColumnInfo (name = "date")
+    public String date;
+
+    @ColumnInfo (name = "person_id")
+    public int personID;
+
+    @ColumnInfo (name = "text")
+    public String text;
+
+    @ColumnInfo (name = "type_id")
+    public int typeID;
+}
