3b441e57d4b8734c2bd2ee8b80f407e7cd17dee7
amcneil <amcneil@mymail.mines.edu>
2019-11-05 10:13:24 -0700

    Added dropdowns to select a relationship in a selected person, doesn't add relationship to database

--- LifeScribeV1/app/src/main/java/com/example/lifescribev01/SelectedPerson.java
+++ LifeScribeV1/app/src/main/java/com/example/lifescribev01/SelectedPerson.java
@@ -1,5 +1,6 @@
 package com.example.lifescribev01;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.example.lifescribev01.database.AppDatabase;
@@ -11,9 +12,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.text.Editable;
+import android.util.Pair;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class SelectedPerson extends AppCompatActivity {
 
@@ -26,7 +34,7 @@ public class SelectedPerson extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         Bundle b = getIntent().getExtras();
-        int id = b.getInt("id");
+        final int id = b.getInt("id");
 
         AppDatabase appDb = MainActivity.GetDatabase();
         Person p = appDb.personDao().findByID(id);
@@ -39,6 +47,41 @@ public class SelectedPerson extends AppCompatActivity {
         dodField.setText(p.deathDate);
         EditText bioField = findViewById(R.id.bio);
         bioField.setText(p.bio);
+
+        final Spinner relatives= findViewById(R.id.relative);
+        List<Person> dbPeople = appDb.personDao().getAll();
+        List<String> pList = new ArrayList<>();
+        for (Person pe: dbPeople) {
+            pList.add(pe.name);
+        }
+        ArrayAdapter<String> relativeAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, pList);
+        relatives.setAdapter(relativeAdapter);
+
+        List<String> rList = new ArrayList<String>(){{
+            add("Parent");
+            add("Child");
+            add("Sibling");
+        }};
+        final Spinner relationships= findViewById(R.id.relationship);
+        ArrayAdapter<String> relationshipAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, rList);
+        relationships.setAdapter(relationshipAdapter);
+
+
+        Button submit = findViewById(R.id.submit);
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final Spinner personField = findViewById(R.id.relative);
+                final Spinner relationshipField = findViewById(R.id.relationship);
+                int personID = personField.getSelectedItemPosition() + 1;
+                Pair peoplePair;
+                String rel = relationshipField.getSelectedItem().toString();
+                if(rel == "Parent"){
+                    peoplePair = new Pair(id, personID);
+                }
+
+            }
+        });
     }
 
 }
