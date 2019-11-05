774d93a0cdac799ae441241488ebefcd6de890e0
Daniel Thorne <thorne@mymail.mines.edu>
2019-11-05 11:04:25 -0700

    added update method

--- LifeScribeV1/app/src/main/java/com/example/lifescribev01/database/SiblingXRefDao.java
+++ LifeScribeV1/app/src/main/java/com/example/lifescribev01/database/SiblingXRefDao.java
@@ -12,5 +12,5 @@ public interface SiblingXRefDao {
     void insert(SiblingXRef pair);
 
     @Query("SELECT * FROM person INNER JOIN SIBLING_XREF ON person.person_id = sibling_xref.sibling_2_id WHERE sibling_xref.sibling_1_id=:siblingID")
-    List<Person> getParentsOfPerson(int siblingID);
+    List<Person> getSiblingOfPerson(int siblingID);
 }
